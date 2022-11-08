e6e5d292b430f74cbc899bf879fa9ac099ae5df5
bayraaaaaaa <bayraanix123@gmail.com>
2022-11-08 18:38:26 +0800

    nest

---
+++ ButtonDisableAndGridFolder/buttondisableandgrid/src/App.jsx
@@ -0,0 +1,14 @@
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+function App() {
+
+    return (
+        <>
+            <Button variant="primary">Primary</Button>{' '}
+            <Button variant="dark">Active</Button>{' '}
+            <Button variant="danger">Disable</Button>{' '}
+        </>
+    );
+}
+
+export default App;
